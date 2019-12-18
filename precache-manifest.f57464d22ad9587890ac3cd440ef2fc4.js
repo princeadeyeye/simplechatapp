4eb56127b0e408f66e94ea4cf6c0b371b5f0ddfb
Muiz Adeyeye <muizadeyeye@hmail.com>
2019-12-18 06:31:49 +0100

    Updates

---
+++ precache-manifest.f57464d22ad9587890ac3cd440ef2fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b034a5bf86d1c91a17b69e79e3c15d82",
+    "url": "/simplechatapp/index.html"
+  },
+  {
+    "revision": "fc8d0483fea5e067d358",
+    "url": "/simplechatapp/static/css/main.485eb24e.chunk.css"
+  },
+  {
+    "revision": "593d18e558c1231c4b7c",
+    "url": "/simplechatapp/static/js/2.d525b038.chunk.js"
+  },
+  {
+    "revision": "507a7370b52a72b5f39c2fecbeb4dc40",
+    "url": "/simplechatapp/static/js/2.d525b038.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc8d0483fea5e067d358",
+    "url": "/simplechatapp/static/js/main.96dfa5a0.chunk.js"
+  },
+  {
+    "revision": "27ffe1ec8533e5d13e3a",
+    "url": "/simplechatapp/static/js/runtime-main.31d61d42.js"
+  }
+]);
